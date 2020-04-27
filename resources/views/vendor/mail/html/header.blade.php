ec9a5963ebdef3905ad7c0c861b4c5e28309a79f
sekonioluwaseun@gmail.com <sekonioluwaseun@gmail.com>
2020-04-27 14:15:06 +0100

    Updated some part of the content showing details of a request

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ url('/') }}" style="display: inline-block;">
 @if (trim($slot) === 'LockdownClerk')
-<img src="{{ asset('white/img/logo_group.png') }}" class="logo" alt="LockdownClerk Logo">
+<img src="{{ asset('white/img/lc_logo.png') }}" class="logo" alt="LockdownClerk Logo">
 @else
 {{ $slot }}
 @endif
