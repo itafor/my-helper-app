ec77c62deb14c1dabe475e7f2dbc88938a2c23cf
francis <itaforfrancis@gmail.com>
2020-07-17 23:09:22 -0700

    app.blade updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRequest;
+use App\LockdownRequest;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -18,5 +19,14 @@ class UserController extends Controller
     public function index(User $model)
     {
         return view('users.index', ['users' => $model->paginate(15)]);
+    }
+
+    public function user_requests(Request $request){
+
+              $data['userRequests'] = LockdownRequest::where([
+                ['user_id',authUser()->id]
+              ])->orderBy('created_at', 'DESC')->get();
+
+        return view('users.requests', $data);
     }
 }
