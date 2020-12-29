f52f30e78d61c0abc817a657b2cb5b2817eaf7e2
francis <itaforfrancis@gmail.com>
2020-12-28 17:06:21 -0800

    Implementing Item and its categories

---
+++ app/ItemSubCategory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ItemSubCategory extends Model
+{
+
+
+protected $fillable = ['category_id','name'];
+
+public function item_category()
+{
+	return $this->belongsTo(Category::class,'category_id','id');
+}
+
+    public static function createNew($data)
+  {
+  	
+      foreach ($data['subcategories'] as $key => $subcategory) {
+        $subcategory = self::create([
+           'name' => $subcategory['name'],
+           'category_id' => $data['category_id'],
+        ]); 
+      }
+
+        return $subcategory;
+    }
+
+  public static function updateSubCategory($data)
+    {
+
+     $item_subcategory  =  self::where([
+            ['id', $data['adSubCategoryId'] ],
+        ])->update([
+           'category_id' => $data['category_id'],
+           'name' => $data['name'],
+        ]); 
+
+        return $item_subcategory;
+
+    }
+}
--- resources/views/admin/product_subcategory/create.blade.php
+++ resources/views/admin/product_subcategory/create.blade.php
@@ -1,5 +1,5 @@
 
-@extends('admin.layouts.master',['pageSlug' => 'products_services'])
+@extends('admin.layouts.master',['pageSlug' => 'item_category'])
 
 
 
@@ -16,21 +16,26 @@ Admin | Logistic Agents
           <div class="col-md-8">
             <div class="card">
               <div class="card-header">
-                <h5 class="title float-left">Add New Item Category</h5>
+                <h5 class="title float-left">Item: Add New</h5>
                 <h5 class="title float-right">
                 </h5>
               </div>
               <br>
               <br>
               <div class="card-body">
-                <form class="form" method="post" action="{{ route('admin.product.store') }}">
+                <form class="form" method="post" action="{{ route('admin.item.Subcategory.store') }}">
                     @csrf
 
                       <div class="row">
                     <div class="col-md-12">
                       <div class="form-group">
-                        <label>Item Category name</label>
-                        <input type="text" name="title" class="form-control" placeholder="Item Category Name" required>
+                        <label>Item Category</label>
+                        <select name="category_id" class="form-control" required>
+                          <option>Select a Item category</option>
+                          @foreach($items as $item)
+                          <option value="{{$item->id}}">{{$item->title}}</option>
+                          @endforeach
+                        </select>
                           @error('title')
                     <small style="color: red; font-size: 14px;"> {{ $message }}</small>
                     @enderror
@@ -38,18 +43,21 @@ Admin | Logistic Agents
                     </div>
                   </div>
               
-                   
-                    <div class="row">
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label>Item description</label>
-                        <textarea type="text" name="description" class="form-control" placeholder="Item Category description"></textarea>
-                          @error('description')
-                    <small style="color: red; font-size: 14px;"> {{ $message }}</small>
-                    @enderror
-                      </div>
-                    </div>
-                  </div>
+
+                 <div class="col-md-12">
+                  <label class="form-control-label" for="input-property_type">{{ __('Item') }}</label>
+                  <input type="text" name="subcategories[112211][name]"  class="form-control" required>
+                </div>
+
+                <div style="clear:both"></div>
+                <div id="subcaegoryContainer" class="col-md-12">
+                </div>   
+                <div style="clear:both"></div>
+
+                   <div class="form-group">
+                  <button type="button" id="addMoreSubcategory" class="btn btn-primary btn-sm"><i class="fa fa-plus-circle"></i>  Add more items</button>
+                </div>
+
                   <div class="row">
                     <div class="col-md-12">
                       <div class="form-group">
